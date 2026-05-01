c7b4a72ef3c9d800999af436233222371051c7e4
sam02l <samuelduarteherrera@cedoc.edu.co>
2026-04-30 20:23:17 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "rfid-app-v1";
+const CACHE_NAME = "rfid-app-v2";
 
 const urlsToCache = [
   "./",
@@ -8,26 +8,16 @@ const urlsToCache = [
   "./icono-512.png"
 ];
 
-/* Instalación */
 self.addEventListener("install", event => {
-  console.log("Service Worker instalado");
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-/* Activación */
-self.addEventListener("activate", event => {
-  console.log("Service Worker activado");
-});
-
-/* Fetch (offline support) */
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+      .then(response => response || fetch(event.request))
   );
 });
