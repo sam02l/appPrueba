a2d4e09e9d496e253345681606867c9fdc637713
sam02l <samuelduarteherrera@cedoc.edu.co>
2026-05-02 16:13:53 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("app-v1").then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/app.js"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
